3a782082f30afd81ab52b693c56d4333fc9c4e19
joeverson santos <joerverson.santos@gmail.com>
2014-10-27 23:27:00 -0200

    aparence and init functions

--- server/soket.communication.php
+++ server/soket.communication.php
@@ -1,10 +1,17 @@
 <?
-    $title = $_POST['title-tasks'];
-    $date = $_POST['date-tasks'];
-    $desc = $_POST['desc-tasks'];
-    $pri = $_POST['priority-tasks'];
+    if(!empty($_POST) && $_POST['flag'] == "add"){
+        $title = $_POST['title-tasks'];
+        $date = $_POST['date-tasks'];
+        $desc = $_POST['desc-tasks'];
+        $priority = $_POST['priority-tasks'];
 
-    $conn = new PDO('mysql:host=mysql.hostinger.com.br;dbname=u699756447_gtask',"u699756447_gtask", "joe7895214+");
+        //$conn = new PDO('mysql:host=mysql.hostinger.com.br;dbname=u699756447_gtask',"u699756447_gtask", "joe7895214+");
 
-    $sql = $conn->prepare("");
-?>
+        $con2 = mysql_connect("localhost","root","");
+        mysql_db_name($con2,"gtasks");
+        mysql_query("insert into tasks VALUES ('$title','$date','$desc','$priority')");
+        echo "Pronto";
+    }else{
+        echo "Deu pal ae no envio";
+    }
+echo "oi sou eu aqui";
