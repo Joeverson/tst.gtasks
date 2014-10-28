dfecb352628a9d1785df1a5c51272ca02a286986
joeverson santos <joerverson.santos@gmail.com>
2014-10-27 23:23:56 -0200

    aparence and init functions

--- www/js/index.js
+++ www/js/index.js
@@ -12,4 +12,22 @@ function onDeviceReady() {
     jQuery( window ).on( "swiperight", function( event ) {
         $('.form-box-ini').css({transform:'translateX(0px)'});
     })
+
+    $("input[type=button]").click(function(){
+        var prio = $(this).attr('data-priority');
+        var title = $('#title-task').val();
+        var date = $('#date-task').val();
+        var desc = $('#desc-task').val();
+
+        $.ajax({
+            type: "post",
+            url: "http://127.0.0.1/gtask/soket.communication.php",
+            data: "?title-task="+title+"&date-task="+date+"&desc-task="+desc+"&priority-task="+prio+"&flag=add",
+            datatype: "html"
+        }).done(function(e){
+            alert(e);
+        }).fail(function(e){
+            console.log(e);
+        });
+    });
 }
